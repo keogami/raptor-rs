4c3d8a67ac6f9d914946fe0436916ffffdff057e
keogami <41939011+keogami@users.noreply.github.com>
2026-01-09 17:45:42 +0530

    feat: complete a verbatim implementation

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,9 @@ pub trait Timetable {
     type Stop: Ord + Copy;
     type Route: Ord + Copy;
     type Transfer;
+    type Trip: Copy;
 
+    // TODO: replace vec with cow or iter
     fn get_routes_serving_stop(&self, stop: Self::Stop) -> Vec<Self::Route>;
     fn get_earlier_stop(
         &self,
@@ -19,8 +21,17 @@ pub trait Timetable {
         left: Self::Stop,
         right: Self::Stop,
     ) -> Self::Stop;
+    // TODO: replace vec with cow or iter
     fn get_stops_after(&self, route: Self::Route, stop: Self::Stop) -> Vec<Self::Stop>;
-    fn get_next_arrival(&self, current_trip: Tau, stop: Self::Stop) -> Tau;
+    fn get_earliest_trip(&self, route: Self::Route, stop: Self::Stop) -> Option<Self::Trip>;
+    fn get_arrival_time(&self, trip: Self::Trip, stop: Self::Stop) -> Tau;
+    fn get_departure_time(&self, trip: Self::Trip, stop: Self::Stop) -> Tau;
+    // TODO: replace vec with cow or iter
+    fn get_footpaths_from(&self, stop: Self::Stop) -> Vec<Self::Stop>;
+    fn get_transfer_time(&self, from: Self::Stop, to: Self::Stop) -> Tau {
+        let (_, _) = (from, to);
+        1
+    }
 
     fn raptor(&self, transfers: usize, tau: usize, ps: Self::Stop, pt: Self::Stop) {
         // for (i, stop) earliest known arrival time at `stop` with at most `i` transfers
@@ -37,6 +48,7 @@ pub trait Timetable {
 
         for k in 1..transfers {
             Q.clear();
+            // find all routes that serve the marked stops, for evaluation in this round
             for &marked_stop in &marked_stops {
                 for route in self.get_routes_serving_stop(marked_stop) {
                     let p_dash = Q.entry(route).or_insert(marked_stop);
@@ -47,29 +59,47 @@ pub trait Timetable {
 
             marked_stops.clear();
 
-            // this section is shaky
-            // TODO: confirm the relationship between t_arr(t, p) and et(r, p)
-            // and figure out how to model it in this trait
+            // scanning each route
             for (&route, &p) in Q.iter() {
-                let mut t: Option<Tau> = None;
+                let mut current_trip: Option<Self::Trip> = None;
 
-                for p_i in self.get_stops_after(route, p) {
-                    let best_arrival_to_target = best_arrival.get(&pt).unwrap_or(&usize::MAX);
-                    // TODO: redo this stinking ass if
-                    if t.is_some_and(|t| {
-                        self.get_next_arrival(t, p_i)
-                            < *best_arrival
-                                .get(&p_i)
-                                .unwrap_or(&usize::MAX)
-                                .min(best_arrival_to_target)
-                    }) {
-                        let next_arrival = self.get_next_arrival(t.unwrap(), p_i);
-                        best_arrival_per_k.insert((k, p_i), next_arrival);
-                        best_arrival.insert(p_i, next_arrival);
-                        marked_stops.insert(p_i);
+                for pi in self.get_stops_after(route, p) {
+                    if let Some(arr) = current_trip.map(|trip| self.get_arrival_time(trip, pi)) {
+                        let best_arrival_to_target = best_arrival.get(&pt).unwrap_or(&Tau::MAX);
+                        let best_arrival_to_pi = best_arrival.get(&pi).unwrap_or(&Tau::MAX);
+                        let time_to_beat = *best_arrival_to_pi.min(best_arrival_to_target);
+
+                        if arr < time_to_beat {
+                            best_arrival_per_k.insert((k, pi), arr);
+                            best_arrival.insert(pi, arr);
+                            marked_stops.insert(pi);
+                        }
+                    }
+
+                    if *best_arrival_per_k.get(&(k - 1, pi)).unwrap_or(&Tau::MAX)
+                        <= current_trip
+                            .map(|trip| self.get_departure_time(trip, pi))
+                            .unwrap_or(Tau::MAX)
+                    {
+                        current_trip = self.get_earliest_trip(route, pi);
                     }
                 }
             }
+
+            // look at footpaths, and mark the stops reachable
+            let mut more_marked_stops = Vec::new();
+            for &stop in &marked_stops {
+                for &p_dash in &self.get_footpaths_from(stop) {
+                    let tau = *best_arrival_per_k
+                        .get(&(k, p_dash))
+                        .unwrap_or(&Tau::MAX)
+                        .min(best_arrival_per_k.get(&(k, stop)).unwrap_or(&Tau::MAX));
+                    best_arrival_per_k.insert((k, p_dash), tau);
+                    more_marked_stops.push(p_dash);
+                }
+            }
+
+            marked_stops.extend(&more_marked_stops);
         }
     }
 }
